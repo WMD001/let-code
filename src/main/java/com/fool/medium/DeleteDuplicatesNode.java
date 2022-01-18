ce7d94b0d16ee55e6a726db50152c474881b8c0e
walle <habitdev@163.com>
2022-01-18 16:16:20 +0800

    83. 删除排序链表中的重复元素

---
+++ src/main/java/com/fool/medium/DeleteDuplicatesNode.java
@@ -0,0 +1,27 @@
+package com.fool.medium;
+
+import com.fool.common.ListNode;
+
+
+/**
+ * @author Qiq
+ * @date 2022/1/18
+ */
+public class DeleteDuplicatesNode {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode result = new ListNode(0);
+        result.next = head;
+        ListNode temp = result;
+        while (temp.next != null && temp.next.next != null) {
+            if (temp.next.val == temp.next.next.val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return result.next;
+    }
+
+
+}
